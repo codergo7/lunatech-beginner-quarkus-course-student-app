97a091a6f2271f716998c884bbf5b680fbf91bfa
codergo7 <codergo7@gmail.com>
2022-04-13 18:56:28 +0200

    completed

--- src/main/java/com/lunatech/training/quarkus/GreetingResource.java
+++ src/main/java/com/lunatech/training/quarkus/GreetingResource.java
@@ -2,6 +2,7 @@ package com.lunatech.training.quarkus;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -12,7 +13,12 @@ public class GreetingResource {
     @GET
     @Path("greet")
     public String greet() {
-        return "Hello, Quarkians!";
+        return "Hello Han, Quarkians!";
     }
 
+    @GET
+    @Path("/hello/{subject}")
+    public String getHello(@PathParam("subject") String subject){
+        return "Hello " + subject;
+    }
 }
